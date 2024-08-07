4ea228b0b6ca561900f71cebeca27d3b47e72e56
rajmanna <raj2021manna@gmail.com>
2024-08-07 18:47:40 +0530

    added create todo func

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { readFileSync, writeFile } from 'fs';
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,36 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const readTodos = () => {
+  const todoFile = readFileSync('todo.json', 'utf8');
+  return JSON.parse(todoFile);
+};
+
+const writeTodos = todos => {
+  writeFile('todo.json', JSON.stringify(todos, null, 2), err => {
+    if (err) throw err;
+    console.log('Todo added successfully.');
+  });
+};
+
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  let todoObj = readTodos();
+  res.render('index.ejs', { todo: todoObj });
+});
+
+app.post('/todo', (req, res) => {
+  let todoObj = readTodos();
+  let todoInput = req.body['todo-input'];
+  if (todoInput && todoInput != ' ') {
+    let newTodo = {
+      todoId: Date.now(),
+      todoTitle: todoInput,
+      todoStatus: false,
+    };
+    todoObj.push(newTodo);
+    writeTodos(todoObj);
+  }
+  res.redirect('/');
 });
 
 app.listen(port, () => {
